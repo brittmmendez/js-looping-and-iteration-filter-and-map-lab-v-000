08759e8d2386cf2c1630e308dce78e017e1481b4
Brittany Morris <brittmmendez@gmail.com>
2018-03-16 21:36:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ function driverNamesWithRevenueOver(drivers, revenue){
   array=drivers.filter(function (driver){return driver.revenue>revenue;});
   return array.map(function(driver) {return driver.name;});
 }
+
+function driversWithRevenueOver(drivers, revenue){
+  return drivers.filter(function (driver){
+    return driver.revenue>revenue;
+  });
+}
+
+function exactMatch (drivers, option){
+  const key=Object.keys(option);
+  const attribute = (option[key]);
+ 
+  return drivers.filter(function (driver){
+    return driver.name===attribute || driver.revenue===attribute;
+  });
+}
